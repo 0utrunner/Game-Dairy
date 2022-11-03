fea5bcea8358140d54976e7ef1ecf624498dd659
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-11-03 15:13:06 -0500

    initial commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+
+  // vite uses this as a prefix for href and src URLs
+  base: '/static/',
+  build: {
+    // this is the folder where vite will generate its output. Make sure django can serve files from here!
+    outDir: '../videogames_proj/static',
+
+    // delete the old build when creating the new build. 
+    // this is the default behavior, unless outDir is outside of the current directory
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  plugins: [react()]
+})
---
+++ videogames_proj/videogames_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VideogamesAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'videogames_app'
