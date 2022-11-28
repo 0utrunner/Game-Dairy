47aa16ef2761e2e4983659498d831228f2bc7ff9
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-11-27 22:34:09 -0600

    started on navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
+import SiteNav from './components/SiteNav'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   console.log("Let's get started then...")
   return (
-    <div className="App">
-      <h1>The Games That Made Me</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+    <div className="Site">
+      <h1>The Games that Made Me</h1>
     </div>
   )
 }
---
+++ frontend/src/components/SiteNav.jsx
@@ -0,0 +1,14 @@
+
+
+function SiteNav() {
+
+    return(
+        <Navbar bg="light" variant="light">
+            <Container>
+            <Navbar.Brand>The Games That Made Me</Navbar.Brand>
+            </Container>
+      </Navbar>
+    )
+}
+
+export default SiteNav;
