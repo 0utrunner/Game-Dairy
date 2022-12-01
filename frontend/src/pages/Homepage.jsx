b73cf025850741e073ee504e33e09fb80d8aaa47
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-11-30 21:03:33 -0600

    revised main goal of the project

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -4,11 +4,8 @@ function Homepage() {
 
     return (
         <div>
-            <h1>The Games That Made Me</h1>
-                <p>I was a military brat growing up, so my family was always on the go. The process was always repeating, make new friends,
-                4 years later say goodbye to those friends, and continue the cycle. One thing never left me though, the video games I owned.
-                These were like friends I could visit anytime I wanted, and were always up to keep me company, and a lot of them left lasting
-                impressions on me and even influenced how I act to this day. This site is a thank you letter to the games that made me.</p>
+            <h1>The Game Dairy</h1>
+                <p>This is a website for me to talk about all the games I've played and loved through my life. Updated whenever I feel like talking about another game.</p>
         </div>
     );
 }
