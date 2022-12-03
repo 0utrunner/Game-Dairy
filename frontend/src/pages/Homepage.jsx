752c9d802a0d4381d0bcca897d575531a0d74c49
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-12-02 21:27:57 -0600

    quick fix

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -4,7 +4,7 @@ function Homepage() {
 
     return (
         <div>
-            <h1>The Game Dairy</h1>
+            <h1>The Game Diary</h1>
                 <p>This is a website for me to talk about all the games I've played and loved through my life. Updated whenever I feel like talking about another game.</p>
         </div>
     );
