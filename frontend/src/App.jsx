aeef5e77d7fc109a75851873be2b6198f543414c
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-12-05 21:04:21 -0600

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,11 @@ import SiteNav from './components/SiteNav'
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-
+  
   console.log("Let's get started then...")
   return (
     <div>
+      <SiteNav />
       <Router>
         <Routes>
           <Route path='/' element={<Homepage />}/>
--- frontend/src/components/SiteNav.jsx
+++ frontend/src/components/SiteNav.jsx
@@ -1,13 +1,22 @@
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function SiteNav() {
 
     return(
-        <Navbar bg="light" variant="light">
-            <Container>
-            <Navbar.Brand>The Games That Made Me</Navbar.Brand>
-            </Container>
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand>The Game Diary</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#">All Games</Nav.Link>
+            <Nav.Link href="#">Games by Console</Nav.Link>
+            <Nav.Link href="#">Games by Genre</Nav.Link>
+          </Nav>
+        </Container>
       </Navbar>
+    </>
     )
 }
 
